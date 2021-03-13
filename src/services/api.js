433a67e1fece87bf1ab1b0a914af580e168b5808
Juan Dantas <rianlandim22@gmail.com>
2021-03-13 03:30:51 -0300

    ApiGoogleNews

---
+++ src/services/api.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+export const key = "77d1c7a9ffef4ec2bf23b2ec633f2537"
+
+const api = axios.create({
+        baseURL:"http://newsapi.org/v2/"
+    })
+
+export default api;
